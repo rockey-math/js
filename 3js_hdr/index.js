018b8025c199a705f47ba6662f56d580ea91cbea
rockey-math <112349400+rockey-math@users.noreply.github.com>
2022-09-04 20:46:57 -0400

    Update index.js

--- 3js_hdr/index.js
+++ 3js_hdr/index.js
@@ -1,4 +1,5 @@
-		        import * as THREE from 'https://threejs.org/examples/../build/three.module.js';
+		        // import * as THREE from 'https://threejs.org/examples/../build/three.module.js';
+                        import * as THREE from 'https://threejs.org/build/three.js';
 			import { RGBELoader } from './jsm/loaders/RGBELoader.js';
 			import { RoughnessMipmapper } from './jsm/utils/RoughnessMipmapper.js';
                         RGBELoader('Modules are pretty cool.');
