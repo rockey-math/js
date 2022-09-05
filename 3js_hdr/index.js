cd1bfded77f62eb958365f7988ae0f492d9482eb
rockey-math <112349400+rockey-math@users.noreply.github.com>
2022-09-04 20:03:22 -0400

    Create index.js

---
+++ 3js_hdr/index.js
@@ -0,0 +1,7 @@
+		<script>
+			import { RGBELoader } from './jsm/loaders/RGBELoader.js';
+			import { RoughnessMipmapper } from './jsm/utils/RoughnessMipmapper.js';
+			//  import {addTextToBody} from './utils.mjs';
+
+                         RGBELoader('Modules are pretty cool.');
+</script>
