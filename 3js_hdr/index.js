d479f728c94324854d3291093cd464a5878a6aac
rockey-math <112349400+rockey-math@users.noreply.github.com>
2022-09-04 20:26:27 -0400

    Update index.js

--- 3js_hdr/index.js
+++ 3js_hdr/index.js
@@ -1,7 +1,6 @@
 		
 			import { RGBELoader } from './jsm/loaders/RGBELoader.js';
 			import { RoughnessMipmapper } from './jsm/utils/RoughnessMipmapper.js';
-			//  import {addTextToBody} from './utils.mjs';
-
-                         RGBELoader('Modules are pretty cool.');
+                        RGBELoader('Modules are pretty cool.');
+                        console.log("Hello")
 
